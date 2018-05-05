ddaec4d12985d6d9d899535c619f99f945a6006c
WeilonYing <weilony@hotmail.com>
2018-05-06 00:23:22 +1000

    Revert previous 3 commits. They are now in basetemplates branch.

--- compclub/website/urls.py
+++ compclub/website/urls.py
@@ -3,11 +3,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index, name='home'),
     path('events/', views.event_index, name='event_index'),
     path('events/<event_url_name>/', views.event_page, name='event_page'),
     path('about/', views.about, name='about'),
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.login, name='signup')
+    path('', views.index, name='index')
 ]
