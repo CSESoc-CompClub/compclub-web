6482a614f434ae67967c488df56924032e0fbf13
mehri-amin <mehriamin5@gmail.com.au>
2018-05-05 22:39:10 +1000

    Fixed paths in index template

--- compclub/website/urls.py
+++ compclub/website/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('events/', views.event_index, name='event_index'),
     path('events/<event_url_name>/', views.event_page, name='event_page'),
     path('about/', views.about, name='about'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.login, name='signup')
 ]
