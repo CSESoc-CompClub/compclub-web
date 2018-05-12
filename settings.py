a20de689f46052b49a8a80a910bf6dae2a3a1418
Nick Whyte <nick@nickwhyte.com>
2018-05-13 01:42:45 +1000

    Get deploy-ready with Docker and Gunicorn (#7)

--- settings.py
+++ settings.py
@@ -13,23 +13,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-env = os.environ.get('ENV', 'development').lower()
-if env == 'production':
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = os.environ["SECRET_KEY"]
-    DEBUG = False
-else:
-    SECRET_KEY = 'development-only'
-    DEBUG = True # SECURITY WARNING: don't run with debug turned on in production!
-
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,18 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'compclub.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
---
+++ settings_dev.py
@@ -0,0 +1,12 @@
+from settings import *
+import os
+
+SECRET_KEY = 'development-only'
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
