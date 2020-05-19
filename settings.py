9e8600aa1d446a079d90b5be7e2214eaa7808ecd
gawdn <26182503+gawdn@users.noreply.github.com>
2020-05-19 17:43:57 +1000

    fix lint

--- settings.py
+++ settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-from django.conf.global_settings import STATICFILES_FINDERS
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
